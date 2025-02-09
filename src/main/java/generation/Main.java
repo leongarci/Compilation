8db3e958c2c4702cd1e4b5b7e6c5aadc09378da9
leongarci <leongarcia1669@gmail.com>
2025-02-09 19:41:33 +0100

    Exemple6
    Exemple des conditionnelles

--- src/main/java/generation/Main.java
+++ src/main/java/generation/Main.java
@@ -1,20 +1,67 @@
 package generation;
 
 import fr.ul.miashs.compil.arbre.*;
+import tds.TableDesSymboles;
+import tds.Entree;
 
 public class Main {
     public static void main(String[] args) {
         // Crée le programme principal
         Prog programme = new Prog();
 
-        // Crée la fonction principale "main"
-        Fonction mainFonction = new Fonction("main");
+        // Crée la fonction g avec deux paramètres
+        Fonction fonctionG = new Fonction("g");
+        Bloc blocG = new Bloc();
 
-        // Ajoute la fonction main au programme
-        programme.ajouterUnFils(mainFonction);
+        // Ajouter les paramètres à la TDS
+        GenerateurCode generateur = new GenerateurCode();
+        Entree paramA = new Entree("a", "int", "param");
+        paramA.setRang(0);
+        paramA.setScope("g");
+        Entree paramB = new Entree("b", "int", "param");
+        paramB.setRang(1);
+        paramB.setScope("g");
+
+        generateur.getTds().ajouterSymbole("a", paramA);
+        generateur.getTds().ajouterSymbole("b", paramB);
+
+        // Construire une conditionnelle : if (a > b) x = a; else x = b;
+        Superieur condition = new Superieur();
+        condition.setFilsGauche(new Idf("a"));
+        condition.setFilsDroit(new Idf("b"));
+
+        Bloc blocAlors = new Bloc();
+        Affectation affectationXAlors = new Affectation();
+        affectationXAlors.setFilsGauche(new Idf("x"));
+        affectationXAlors.setFilsDroit(new Idf("a"));
+        blocAlors.ajouterUnFils(affectationXAlors);
+
+        Bloc blocSinon = new Bloc();
+        Affectation affectationXSinon = new Affectation();
+        affectationXSinon.setFilsGauche(new Idf("x"));
+        affectationXSinon.setFilsDroit(new Idf("b"));
+        blocSinon.ajouterUnFils(affectationXSinon);
+
+        Si conditionnelle = new Si();
+        conditionnelle.setCondition(condition);
+        conditionnelle.setBlocAlors(blocAlors);
+        conditionnelle.setBlocSinon(blocSinon);
+
+        // Ajouter la conditionnelle au bloc de la fonction g
+        blocG.ajouterUnFils(conditionnelle);
+
+        // Ajouter le bloc à la fonction g
+        fonctionG.ajouterUnFils(blocG);
+
+        // Ajouter la fonction g au programme
+        programme.ajouterUnFils(fonctionG);
+
+        // Vérification : l'arbre abstrait est-il complet ?
+        if (programme.getFils().isEmpty()) {
+            throw new RuntimeException("L'arbre abstrait est vide ou incomplet !");
+        }
 
         // Affiche l'arbre abstrait
-        GenerateurCode generateur = new GenerateurCode();
         generateur.afficherArbre(programme);
 
         // Génère le code assembleur
