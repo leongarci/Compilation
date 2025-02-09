8d7e1b80ca626796262046ece4c1d8e3942fa7a1
leongarci <leongarcia1669@gmail.com>
2025-02-09 18:31:19 +0100

    L'exemple 2 avec les variables globales

--- src/main/java/generation/Main.java
+++ src/main/java/generation/Main.java
@@ -1,11 +1,27 @@
 package generation;
 
 import fr.ul.miashs.compil.arbre.*;
+import tds.Entree;
 
 public class Main {
     public static void main(String[] args) {
         // Crée le programme principal
         Prog programme = new Prog();
+
+        // Définir les variables globales
+        Entree varI = new Entree("i", "int", "global");
+        varI.setValeur(10);
+        Entree varJ = new Entree("j", "int", "global");
+        varJ.setValeur(20);
+        Entree varK = new Entree("k", "int", "global");
+        Entree varL = new Entree("l", "int", "global");
+
+        // Ajouter les variables globales à la TDS
+        GenerateurCode generateur = new GenerateurCode();
+        generateur.getTds().ajouterSymbole("i", varI);
+        generateur.getTds().ajouterSymbole("j", varJ);
+        generateur.getTds().ajouterSymbole("k", varK);
+        generateur.getTds().ajouterSymbole("l", varL);
 
         // Crée la fonction principale "main"
         Fonction mainFonction = new Fonction("main");
@@ -14,7 +30,6 @@ public class Main {
         programme.ajouterUnFils(mainFonction);
 
         // Affiche l'arbre abstrait
-        GenerateurCode generateur = new GenerateurCode();
         generateur.afficherArbre(programme);
 
         // Génère le code assembleur
