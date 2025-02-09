f931eb45ac5b1160e40294710e0df44a97dc4ea3
leongarci <leongarcia1669@gmail.com>
2025-02-09 21:28:54 +0100

    Exemple8
    Bah l'exemple 8 quoi

--- src/main/java/generation/Main.java
+++ src/main/java/generation/Main.java
@@ -9,53 +9,69 @@ public class Main {
         // Crée le programme principal
         Prog programme = new Prog();
 
-        // Crée la fonction h avec une variable locale
-        Fonction fonctionH = new Fonction("h");
-        Bloc blocH = new Bloc();
+        // Crée la fonction fact pour calculer la factorielle récursivement
+        Fonction fonctionFact = new Fonction("fact");
+        Bloc blocFact = new Bloc();
 
-        // Ajouter une variable locale à la TDS
+        // Ajouter les paramètres et la variable locale à la TDS
         GenerateurCode generateur = new GenerateurCode();
-        Entree varI = new Entree("i", "int", "local");
-        varI.setScope("h");
+        Entree paramN = new Entree("n", "int", "param");
+        paramN.setRang(0);
+        paramN.setScope("fact");
+        Entree varResult = new Entree("result", "int", "local");
+        varResult.setScope("fact");
 
-        generateur.getTds().ajouterSymbole("i", varI);
+        generateur.getTds().ajouterSymbole("n", paramN);
+        generateur.getTds().ajouterSymbole("result", varResult);
 
-        // Construire une itération : for (i = 0; i < 10; i = i + 1)
-        Affectation initialisation = new Affectation();
-        initialisation.setFilsGauche(new Idf("i"));
-        initialisation.setFilsDroit(new Const(0));
+        // Construire une conditionnelle : if (n <= 1) result = 1; else result = n * fact(n - 1);
+        InferieurEgal condition = new InferieurEgal();
+        condition.setFilsGauche(new Idf("n"));
+        condition.setFilsDroit(new Const(1));
 
-        Inferieur condition = new Inferieur();
-        condition.setFilsGauche(new Idf("i"));
-        condition.setFilsDroit(new Const(10));
+        // Bloc alors : result = 1
+        Bloc blocAlors = new Bloc();
+        Affectation affectationResultAlors = new Affectation();
+        affectationResultAlors.setFilsGauche(new Idf("result"));
+        affectationResultAlors.setFilsDroit(new Const(1));
+        blocAlors.ajouterUnFils(affectationResultAlors);
 
-        Plus incrementation = new Plus();
-        incrementation.setFilsGauche(new Idf("i"));
-        incrementation.setFilsDroit(new Const(1));
+        // Bloc sinon : result = n * fact(n - 1)
+        Bloc blocSinon = new Bloc();
+        Moins decrementation = new Moins();
+        decrementation.setFilsGauche(new Idf("n"));
+        decrementation.setFilsDroit(new Const(1));
 
-        Affectation miseAJour = new Affectation();
-        miseAJour.setFilsGauche(new Idf("i"));
-        miseAJour.setFilsDroit(incrementation);
+        Appel appelFact = new Appel("fact");
+        appelFact.ajouterUnFils(decrementation);
 
-        Bloc blocIteration = new Bloc();
-        Ecrire ecrireI = new Ecrire();
-        ecrireI.setLeFils(new Idf("i"));
-        blocIteration.ajouterUnFils(ecrireI);
+        Multiplication multiplication = new Multiplication();
+        multiplication.setFilsGauche(new Idf("n"));
+        multiplication.setFilsDroit(appelFact);
 
-        TantQue boucle = new TantQue();
-        boucle.setCondition(condition);
-        boucle.setBloc(blocIteration);
+        Affectation affectationResultSinon = new Affectation();
+        affectationResultSinon.setFilsGauche(new Idf("result"));
+        affectationResultSinon.setFilsDroit(multiplication);
+        blocSinon.ajouterUnFils(affectationResultSinon);
 
-        // Ajouter les instructions au bloc de la fonction h
-        blocH.ajouterUnFils(initialisation);
-        blocH.ajouterUnFils(boucle);
-        blocH.ajouterUnFils(miseAJour);
+        // Construire la conditionnelle
+        Si conditionnelle = new Si();
+        conditionnelle.setCondition(condition);
+        conditionnelle.setBlocAlors(blocAlors);
+        conditionnelle.setBlocSinon(blocSinon);
 
-        // Ajouter le bloc à la fonction h
-        fonctionH.ajouterUnFils(blocH);
+        // Construire le retour : return result
+        Retour retourResult = new Retour(new Idf("result"));
 
-        // Ajouter la fonction h au programme
-        programme.ajouterUnFils(fonctionH);
+        // Ajouter les instructions au bloc de la fonction fact
+        blocFact.ajouterUnFils(conditionnelle);
+        blocFact.ajouterUnFils(retourResult);
+
+        // Ajouter le bloc à la fonction fact
+        fonctionFact.ajouterUnFils(blocFact);
+
+        // Ajouter la fonction fact au programme
+        programme.ajouterUnFils(fonctionFact);
 
         // Vérification : l'arbre abstrait est-il complet ?
         if (programme.getFils().isEmpty()) {
@@ -63,7 +79,6 @@ public class Main {
         }
 
         // Affiche l'arbre abstrait
-        generateur.afficherArbre(programme);
 
         // Génère le code assembleur
         String codeAssembleur = generateur.genererProgramme(programme);
