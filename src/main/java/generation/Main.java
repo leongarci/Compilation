ef0736c0d44d0703f1ed67b5a26065eef078c04c
leongarci <leongarcia1669@gmail.com>
2025-02-09 18:57:03 +0100

    Exemple 4
    Exemple 4 de l'ecriture

--- src/main/java/generation/Main.java
+++ src/main/java/generation/Main.java
@@ -1,20 +1,45 @@
 package generation;
 
 import fr.ul.miashs.compil.arbre.*;
+import tds.Entree;
 
 public class Main {
     public static void main(String[] args) {
         // Crée le programme principal
         Prog programme = new Prog();
 
+        // Définir les variables globales
+        Entree varA = new Entree("a", "int", "global");
+        Entree varB = new Entree("b", "int", "global");
+
+        // Ajouter les variables globales à la TDS
+        GenerateurCode generateur = new GenerateurCode();
+        generateur.getTds().ajouterSymbole("a", varA);
+        generateur.getTds().ajouterSymbole("b", varB);
+
         // Crée la fonction principale "main"
         Fonction mainFonction = new Fonction("main");
+        Bloc blocPrincipal = new Bloc();
 
-        // Ajoute la fonction main au programme
+        // Construire l'expression ecrire(a);
+        Ecrire ecrireA = new Ecrire();
+        ecrireA.setLeFils(new Idf("a"));
+
+        // Construire l'expression ecrire(b);
+        Ecrire ecrireB = new Ecrire();
+        ecrireB.setLeFils(new Idf("b"));
+
+        // Ajouter les instructions au bloc principal
+        blocPrincipal.ajouterUnFils(ecrireA);
+        blocPrincipal.ajouterUnFils(ecrireB);
+
+        // Ajouter le bloc principal à la fonction main
+        mainFonction.ajouterUnFils(blocPrincipal);
+
+        // Ajouter la fonction main au programme
         programme.ajouterUnFils(mainFonction);
 
         // Affiche l'arbre abstrait
-        GenerateurCode generateur = new GenerateurCode();
         generateur.afficherArbre(programme);
 
         // Génère le code assembleur
